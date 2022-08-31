34fe7dc5a356fc1fcef9f1f1bedd5e278082cba2
Spprince007 <spprince007@gmail.com>
2022-09-01 04:03:53 +0600

    selected product done

--- js/meals.js
+++ js/meals.js
@@ -1,5 +1,5 @@
 const loadMeals = (search) => {
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayMeals(data.meals))
@@ -9,17 +9,17 @@ const displayMeals = meals => {
     const mealSection = document.getElementById('meal-section');
     mealSection.innerHTML = '';
     meals.forEach(meal => {
-        console.log(meal);
+        //console.log(meal);
         const newDiv = document.createElement('div');
-        newDiv.classList.add('col')
+        newDiv.classList.add('col');
         newDiv.innerHTML = `
-        <div class="card">
+        <div onclick="loadMealDetails('${meal.idMeal}')" class="card">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
             <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
         </div>
-        `
+        `;
 
         mealSection.appendChild(newDiv);
     });
@@ -32,4 +32,26 @@ function searchMilk() {
     inputField.value = '';
 }
 
-// loadMeals(a);
+function loadMealDetails(idMeal) {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+}
+
+function displayMealDetails(meal) {
+    const mealDetails = document.getElementById('meal-details');
+    mealDetails.innerHTML = '';
+    const detailsDiv = document.createElement('div');
+    detailsDiv.classList.add('card')
+    detailsDiv.innerHTML = `
+    
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+   
+    `;
+    mealDetails.appendChild(detailsDiv);
+}
+//loadMeals();
--- random.js
+++ random.js
@@ -1,17 +1,20 @@
-const loadProfile = () => {
-    fetch('https://randomuser.me/api/?results=50')
-        .then(res => res.json())
-        .then(data => profiles(data.results))
-}
-const profiles = users => {
-    const profile = document.getElementById('profile');
-    for (const user of users) {
-        const createADiv = document.createElement('div');
-        createADiv.innerHTML = `
-        <h3>Title: ${user.gender}</h3>
-        `
-        profile.appendChild(createADiv);
-    }
+// const loadProfile = () => {
+//     fetch('https://randomuser.me/api/?results=50')
+//         .then(res => res.json())
+//         .then(data => profiles(data.results))
+// }
+// const profiles = users => {
+//     const profile = document.getElementById('profile');
+//     for (const user of users) {
+//         const createADiv = document.createElement('div');
+//         createADiv.innerHTML = `
+//         <h3>Title: ${user.gender}</h3>
+//         `
+//         profile.appendChild(createADiv);
+//     }
 
 
-}
+// }
+
+const array = { hobbies: ["dancing", "singing", "acting"] };
+console.log(JSON.stringify(array));
